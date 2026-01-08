1c48f778a30d3a37fc8d955e17de95f330b132a9
Peter Tillema <ptillema@quintor.nl>
2026-01-08 12:19:49 +0100

    Don't return out from fadd in mod p, but allow overflow. The output is used for multiplication input, which already handles overflows itself

--- src/x25519.asm
+++ src/x25519.asm
@@ -1,7 +1,7 @@
 INT_SIZE = 32
 P_OFFSET = 19
 
-; Code size: 978 bytes
+; Code size: 962 bytes
 ; Data size: 321 bytes
 ; Read only data size: 64 bytes
 
@@ -61,7 +61,7 @@ _tls_x25519_secret:
 ;   arg3 = their_public
 ;   arg4 = yield_fn
 ;   arg5 = yield_data
-; Timing: 488,612,418 cc
+; Timing: 484,363,098 cc
     ld      iy, 0
     add     iy, sp
     ld      hl, (iy + arg3)
@@ -232,7 +232,7 @@ scalar.size := 5
     ret
 
 _fmul:
-; Performs a multiplication between two big integers mod p, and returns the result in mod p again.
+; Performs a multiplication between two big integers, and returns the result in mod p.
 ; It works because (a mod p) * (b mod p) = (a * b) mod p. The pseudocode for multiplying looks like this:
 ;  - Reset product outcome to zeroes
 ;  - For each byte in input a:
@@ -252,8 +252,8 @@ _fmul:
 ;  - Either out or out - p is used, like in the fadd routine.
 ; Inputs:
 ;   sparg1 = out
-;   sparg2 = a mod p
-;   sparg3 = b mod p
+;   sparg2 = a
+;   sparg3 = b
 ; Size: 213 bytes (D1C620 - D1C54B)
 ; Timing: 152,138 cc
 mul:
@@ -397,7 +397,46 @@ mul.size := 7
     pop     ix
 ; Perform the pack to calculate mod p instead of mod 2p
     ld      hl, _product
-    jq      _calcModP
+    push    hl
+; Subtract p from temp (inline)
+    ld      a, (hl)
+    sub     a, -P_OFFSET
+    ld      (hl), a
+    ld      b, INT_SIZE - 2
+    dec     c               ; c = -1
+.subtractLoop:
+    inc     hl
+    ld      a, (hl)
+    sbc     a, c
+    ld      (hl), a
+    djnz    .subtractLoop
+    inc     hl              ; Same as within the loop, but now 7F to not subtract the last bit
+    ld      a, (hl)
+    sbc     a, 0x7F
+    ld      (hl), a
+    ccf                     ; If the carry flag WAS set, out < p, so no swap needed. Flip the carry flag and call the swap
+    pop     hl              ; hl -> temp
+    jq      _swap
+
+_fadd:
+; Performs an addition between two big integers mod p, and returns the result without any mod. This is possible because
+; the output is used as an input for multiplication, which handles overflows itself.
+; Inputs:
+;   DE = out
+;   BC = a mod p
+;   HL = b mod p
+    xor     a, a            ; Reset carry flag
+    ld      iyl, INT_SIZE
+.addLoop:
+    ld      a, (bc)         ; out[i] = a[i] + b[i] + carry
+    adc     a, (hl)
+    ld      (de), a
+    inc     hl
+    inc     de
+    inc     bc
+    dec     iyl
+    jr      nz, .addLoop
+    ret
 
 _fsub:
 ; Performs a subtraction between two big integers mod p, and returns the result in mod p again.
@@ -445,58 +484,6 @@ _fsub:
     ld      a, (hl)
     adc     a, 0x7F
     ld      (hl), a
-    pop     hl              ; hl -> temp
-    jq      _swap
-
-_fadd:
-; Performs an addition between two big integers mod p, and returns the result in mod p again.
-; It works because (a mod p) + (b mod p) = (a + b) mod p.
-; Inputs:
-;   DE = out
-;   BC = a mod p
-;   HL = b mod p
-; Size: 50 bytes
-; Timing: 3306
-    xor     a, a            ; Reset carry flag
-    ld      iyl, INT_SIZE
-.addLoop:
-    ld      a, (bc)         ; out[i] = a[i] + b[i] + carry
-    adc     a, (hl)
-    ld      (de), a
-    inc     hl
-    inc     de
-    inc     bc
-    dec     iyl
-    jr      nz, .addLoop
-; Now out is in the range [0, 2p-2]. In order to do a mod p, we copy out to a temp variable, subtract p from
-; that and eventually swap places (all in constant time), such that either out or (out - p) is used.
-    dec     de
-    ex      de, hl          ; hl -> out + 31
-    ld      de, _temp + INT_SIZE - 1
-    ld      bc, INT_SIZE
-    lddr
-    ex      de, hl
-    inc     hl              ; hl -> temp
-    inc     de              ; de -> out
-_calcModP:
-    push    hl
-; Subtract p from temp (inline)
-    ld      a, (hl)
-    sub     a, -P_OFFSET
-    ld      (hl), a
-    ld      b, INT_SIZE - 2
-    dec     c               ; c = -1
-.subtractLoop:
-    inc     hl
-    ld      a, (hl)
-    sbc     a, c
-    ld      (hl), a
-    djnz    .subtractLoop
-    inc     hl              ; Same as within the loop, but now 7F to not subtract the last bit
-    ld      a, (hl)
-    sbc     a, 0x7F
-    ld      (hl), a
-    ccf                     ; If the carry flag WAS set, out < p, so no swap needed. Flip the carry flag and call the swap
     pop     hl              ; hl -> temp
 
 _swap:
