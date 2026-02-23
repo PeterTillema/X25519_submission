21e4e134973df52852a4f13eda9355d583433852
Peter Tillema <ptillema@quintor.nl>
2026-02-23 12:48:54 +0100

    Alway allow SMC as the code is already relocated

--- src/x25519.asm
+++ src/x25519.asm
@@ -1,6 +1,5 @@
 INT_SIZE = 32
 P_OFFSET = 19
-ALLOW_SMC = 1
 
 ; Code size: 569 bytes
 ; Relocation size: 1023 bytes
@@ -17,14 +16,6 @@ end namespace
     section .text
     public _tls_x25519_secret
     public _tls_x25519_publickey
-
-macro store_arg_2
-if ALLOW_SMC
-    ld      (_fmul.mulArg2SMC), iy
-else
-    ld      (ix + mul.arg2), iy
-end if
-end macro
 
 arg1 := 3
 arg2 := 6
@@ -67,14 +58,11 @@ _tls_x25519_secret:
 ;   arg4 = yield_fn
 ;   arg5 = yield_data
 ; Timing first attempt: 482,792,828 cc
-; Timing current:       215,960,525 cc      ; Assuming yield_fn = NULL and SMC allowed
+; Timing current:       215,960,525 cc      ; Assuming yield_fn = NULL
 tempVariables:
-scalar:
-scalar.mainLoopIndex := 0                   ; Main loop index
-mul:
-mul.arg2 := 1
+tempVariables.mainLoopIndex := 0                   ; Main loop index
 
-tempVariables.size := 4
+tempVariables.size := 1
 
     push    ix
     ld      ix, -tempVariables.size
@@ -192,12 +180,12 @@ mainCalculationLoop:
     call    _fsubInline
 ; fsquare _d, _e
     ld      iy, _e              ; de -> _d
-    store_arg_2
+    ld      (_fmul.mulArg2SMC), iy
     call    _fmul.start
 ; fsquare _f, _a
     ld      e, _f and 0xFF
     ld      iy, _a
-    store_arg_2
+    ld      (_fmul.mulArg2SMC), iy
     call    _fmul.start
 ; fmul _a, _c, _a
     ld      iy, _c
@@ -223,7 +211,7 @@ mainCalculationLoop:
 ; fsquare _b, _a
     ld      iy, _a
     ld      e, _b and 0xFF
-    store_arg_2
+    ld      (_fmul.mulArg2SMC), iy
     call    _fmul.start
 ; copy _d, _c
     ld      e, _c and 0xFF
@@ -260,7 +248,7 @@ mainCalculationLoop:
 ; fsquare _b, _e
     ld      iy, _e
     ld      e, _b and 0xFF
-    store_arg_2
+    ld      (_fmul.mulArg2SMC), iy
     call    _fmul.start
 ; swap _a, _b
     pop     bc
@@ -287,14 +275,14 @@ mainCalculationLoop:
     ldir
 
 ; Inverse _c
-    ld      (ix + scalar.mainLoopIndex), 254
+    ld      (ix + tempVariables.mainLoopIndex), 254
 .inverseLoop:
 ; fsquare _c, _c
     ld      iy, _c
     ld      e, _c and 0xFF
-    store_arg_2
+    ld      (_fmul.mulArg2SMC), iy
     call    _fmul.start
-    ld      a, (ix + scalar.mainLoopIndex)
+    ld      a, (ix + tempVariables.mainLoopIndex)
     cp      a, 3
     jr      z, .continue2
     cp      a, 5
@@ -305,7 +293,7 @@ mainCalculationLoop:
     ld      l, _b and 0xFF
     call    _fmul
 .continue2:
-    dec     (ix + scalar.mainLoopIndex)
+    dec     (ix + tempVariables.mainLoopIndex)
     jr      nz, .inverseLoop
 
 ; Final multiplication, putting the result in out
@@ -412,11 +400,7 @@ _fmul:
 ;   HL = out + INT_SIZE - 1
 
 ; Copy the input variable to the temporary storage
-if ALLOW_SMC
     ld      (_fmul.mulArg2SMC), hl
-else
-    ld      (ix + mul.arg2), hl
-end if
 .start:
     push    de
 ; Setup the product output
@@ -434,12 +418,8 @@ end if
     inc     hl
     push    hl
     ld      iyh, b
-if ALLOW_SMC
 .mulArg2SMC = $+1
     ld      hl, 0
-else
-    ld      hl, (ix + mul.arg2)
-end if
 ; First iteration
     ld      c, (hl)
     mlt     bc
